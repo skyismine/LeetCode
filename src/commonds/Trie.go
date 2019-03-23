942c2997ec94ad72f8f3c00863c6d6c69c36963d
wuzhangwei <wuzhangwei@ttop9.com>
2019-03-23 14:15:35 +0800

    <feat>(<all>) : 提交 longest-common-prefix 问题自己实现的代码；创建commonds目录用于存放常用数据结构和算法；创建doc/commonds用于存放数据结构和算法的说明文档

---
+++ src/commonds/Trie.go
@@ -0,0 +1,7 @@
+package commonds
+
+/**
+ 字典树结构和实现
+
+ 详细文档参见 doc/commonds/Tire.md
+ */
---
+++ src/problems/LongestCommonPrefix.go
@@ -0,0 +1,93 @@
+package problems
+
+import (
+	"math"
+)
+
+/**
+题目描述:
+编写一个函数来查找字符串数组中的最长公共前缀。
+
+如果不存在公共前缀，返回空字符串 ""。
+
+示例 1:
+
+输入: ["flower","flow","flight"]
+输出: "fl"
+示例 2:
+
+输入: ["dog","racecar","car"]
+输出: ""
+解释: 输入不存在公共前缀。
+说明:
+
+所有输入只包含小写字母 a-z
+
+https://leetcode-cn.com/problems/longest-common-prefix/
+ */
+func LongestCommonPrefix(strs []string, method string) string {
+	switch method {
+	case "mine":
+		return longestCommonPrefixMine(strs)
+	case "lca":
+		return longestCommonPrefixLCA(strs)
+	}
+
+	return ""
+}
+
+/**
+ 先计算字符串数组中最短长度minlen，然后遍历minlen次，对字符串数组中每个字符串比较第i个字符是否相同，如果不同则返回
+
+算法效率：
+   时间复杂度：O(minlen*n) minlen为数组中最短字符串的长度，n为字符串数组的长度
+   空间复杂度: O(len(strs[0])+len(strs[1])+...+len(strs[n]))
+ */
+func longestCommonPrefixMine(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+
+	var minlen int = math.MaxInt32
+	for _, value := range strs {
+		if minlen > len(value) {
+			minlen = len(value)
+		}
+	}
+
+	if minlen == 0 {
+		return ""
+	}
+
+	var ret []byte
+	var firstbytes []byte = []byte(strs[0])
+
+	for i := 0; i < minlen; i++ {
+		for j := 1; j < len(strs); j++ {
+			curr := []byte(strs[j])
+			if firstbytes[i] != curr[i] {
+				return string(ret)
+			}
+		}
+		ret = append(ret, firstbytes[i])
+	}
+	return string(ret)
+}
+
+/**
+ 我们可以通过将所有的键值 S 存储到一颗字典树中来优化最长公共前缀查询操作。 如果你想获得更多关于字典树的信息，可以查看这篇文章 Implement a trie (Prefix trie)[https://leetcode.com/articles/implement-trie-prefix-tree/] 。在字典树中，从根向下的每一个节点都代表一些键值的公共前缀。 但是我们需要找到字符串q 和所有键值字符串的最长公共前缀。 这意味着我们需要从根找到一条最深的路径，满足以下条件：
+
+这是所查询的字符串 q 的一个前缀
+
+路径上的每一个节点都有且仅有一个孩子。 否则，找到的路径就不是所有字符串的公共前缀
+
+路径不包含被标记成某一个键值字符串结尾的节点。 因为最长公共前缀不可能比某个字符串本身长
+
+算法
+
+最后的问题就是如何找到字典树中满足上述所有要求的最深节点。 最有效的方法就是建立一颗包含字符串 [S1...Sn] 的字典树。 然后在这颗树中匹配 q 的前缀。 我们从根节点遍历这颗字典树，直到因为不能满足某个条件而不能再遍历为止。
+
+ */
+func longestCommonPrefixLCA(strs []string) string {
+	return ""
+}
